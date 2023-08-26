42e662439bb841453123f357913eb6737382a719
arunkb2795 <arunkb27@gmail.com>
2023-08-26 09:57:23 +0530

    initial commit

---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+console.log("This file is running in server side",process.argv)
+
+const TodoPage = () => {
+    return (
+        <div>TodoPage</div>
+    )
+}
+
+export default TodoPage
